fcbbbdc3bcdd4de30ba78dbb05cb45660ef2df90
Lovinder Pnag <lspnag@gmail.com>
2016-01-23 19:52:17 -0500

    passing first test but failing others

--- vend_machine.rb
+++ vend_machine.rb
@@ -7,14 +7,17 @@
 # dimes/1/1
 # quarters/3/3
 
+class VendingMachine
 
-def insert_coin(weight, size)
-  okay_weight = [1,2,3].include? weight
-  okay_size = [1,2,3].include? size
-  
-  if okay_weight && okay_size
-    true
-  else
-    false
+  def insert_coin(weight, size)
+    okay_weight = [1,2,3].include? weight
+    okay_size = [1,2,3].include? size
+    
+    if okay_weight && okay_size
+      true
+    else
+      false
+    end
   end
+  
 end
