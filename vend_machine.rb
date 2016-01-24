46e6f1e592936fbeca31c8855bf0fd3289358665
Lovinder Pnag <lspnag@gmail.com>
2016-01-23 19:06:11 -0500

    first test failing

--- vend_machine.rb
+++ vend_machine.rb
@@ -2,6 +2,11 @@
 
 # Accept Coins: nickels, dimes, quarters
 
-def accept_coins
+# coin/weight/size
+# nickels/2/2
+# dimes/1/1
+# quarters/3/3
+
+def insert_coin(weight, size)
   
 end
--- vend_machine_spec.rb
+++ vend_machine_spec.rb
@@ -1,6 +1,11 @@
 require_relative 'vend_machine'
 
-describe 'accept_coins' do
-  it '' do
+describe 'insert_coins' do
+  it 'should accept valid coins: nickels, dimes, quarters' do
+    expect(insert_coin(2,2)).to eq true
+    expect(insert_coin(1,1)).to eq true
+    expect(insert_coin(3,3)).to eq true
   end
 end
+
+# and reject invalid ones
