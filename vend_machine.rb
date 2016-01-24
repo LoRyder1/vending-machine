f11bc3669161567791cecf984d7fa9e22ec98971
Lovinder Pnag <lspnag@gmail.com>
2016-01-23 20:59:11 -0500

    can now read different coins and assign values

--- vend_machine.rb
+++ vend_machine.rb
@@ -11,16 +11,41 @@ class VendingMachine
   attr_reader :current_amount
   def initialize
     @current_amount = 0
+    @weight = nil
+    @size = nil
   end
 
   def validate(weight, size)
+    @weight = weight
+    @size = size
+
     okay_weight = [1,2,3].include? weight
     okay_size = [1,2,3].include? size
     
     if okay_weight && okay_size
+      add_amount
       true
     else
       false
     end
   end
+
+  def add_amount
+    case 
+    when @weight == 2 && @size == 2
+      @current_amount += 5
+    when @weight == 1 && @size == 1
+      @current_amount += 10
+    when @weight == 3 && @size == 3
+      @current_amount += 25
+    end
+  end
 end
+
+
+# x = VendingMachine.new
+
+# x.validate(1,1)
+# x.validate(2,2)
+# x.validate(3,3)
+# p x.current_amount
